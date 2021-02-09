9883abdfcdee482c8397e8540823677dc3661abc
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-02-09 09:11:37 -0300

    Start project layout and starting files

---
+++ index.ts
@@ -0,0 +1,9 @@
+// Find configuration File
+// -- Throw if not found at the root of the project
+
+// Check configuration file types
+// -- Throw if the type is not valid
+
+import { findMetaFunctionFile } from "./src/find-function-declaration-file";
+
+findMetaFunctionFile();
---
+++ src/find-function-declaration-file.ts
@@ -0,0 +1,9 @@
+/**
+ * Finds the `meta-function.json` file
+ * 
+ * This function either finds the file and yields its contents as a string
+ * or throws with an error
+ */
+export const findMetaFunctionFile = () : void => {
+  console.log(process.env.PWD);
+}
