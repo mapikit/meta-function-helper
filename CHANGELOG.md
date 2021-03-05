7d48dcc39f4100f41a86e9a57cb5e4292918675e
Fábio Meneses Junior <fabiomenesesjr@gmail.com>
2021-03-05 09:10:22 -0300

    Add type export

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Releases
+## 0.0.5 - Export types
+- Now the library also exports the types for a valid `meta-function.json`.
+
 ## 0.0.4 - Validate Branch Data & Clouded Objects
 - New validation for presence of data in every branch. Now every branch must have some data bound to it.
 - New Type : `"cloudedObject"` - for when the type is an object, but with unknown properties and keys.
--- index.ts
+++ index.ts
@@ -3,6 +3,22 @@ import { ValidationErrorCodes } from "./src/error-codes";
 import { MetaCustomTypesValidation } from "./src/custom-types-validation";
 import { validateStringConfiguration } from "./src/validate-string-configuration";
 
+import { MetaFunction,
+  AcceptedTypes,
+  CustomType,
+  OutputBranches,
+  OutputData,
+  InputParameters
+} from "./src/meta-function-type";
+
+export { MetaFunction,
+  AcceptedTypes,
+  CustomType,
+  OutputBranches,
+  OutputData,
+  InputParameters
+}
+
 export default {
   validateStringConfiguration,
   MetaCustomTypesValidation,
